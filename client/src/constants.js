35d8352cfe6460409706c698d04d43a4ffa23a62
Olena Zdanovska <freshjs.ftm@gmail.com>
2025-08-06 13:11:24 +0300

    Stripe TEST keys — for demo purposes only

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,6 +4,7 @@ const CONSTANTS = {
   SHIPPING_METHOD: ['free', 'nova post', 'ukr post'],
   SHIPPING_PRICE: { free: 0, 'nova post': 80, 'ukr post': 50 },
   ORDER_STATUS: ['new', 'paid', 'confirm', 'shipped', 'delivered', 'canceled'],
+  // Stripe TEST keys — for demo purposes only
   STRIPE_SECRET_KEY:
     'pk_test_51RrKlARL1VqIp3S3HkswWhggVpWVkbiUdkBxbQURxCt8Iwx8r8JO7mBEeMHviV0kejBXm6dN4FfHSHmQS3hYPIm700i8EpCPGW',
 };
